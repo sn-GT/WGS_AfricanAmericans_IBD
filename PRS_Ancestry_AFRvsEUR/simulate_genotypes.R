3ef7c5ec4f7d61937990c376b0aee3499a2376fd
sn-GT <52112657+sn-GT@users.noreply.github.com>
2022-06-13 16:29:00 -0400

    Add files via upload

--- PRS_Ancestry_AFRvsEUR/simulate_genotypes.R
+++ PRS_Ancestry_AFRvsEUR/simulate_genotypes.R
@@ -1,94 +1,39 @@
-setwd("C:\\Users\\snagpal3\\Dropbox (GaTech)\\IBD\\WGS_AA\\Upsampling_AA")
+
+# Data with SNP,  allele, allele frequency for control individuals
 mydata = read.csv("allele_freq_controls.csv")
 t1 <- (1-mydata$Freq)^2
 t2 <- 1-mydata$Freq^2
 set.seed(7251963)
 
+# Number of individuals to simulate genotypes for
+num_individuals <- 150000
 genolist = list()
-weightlist = list()
-for(i in 1:100000) {
+for(i in 1:num_individuals) {
   n1000 <- runif(1000,0,1)
   geno <- ifelse(n1000<t1, 0, ifelse(n1000<t2,1,2))
-  wgeno <- ifelse(n1000<t1, 0, ifelse(n1000<t2,mydata$LogOdds,2*mydata$LogOdds))
   genolist[[i]] <- geno
-  weightlist[[i]] <- wgeno }
-pop_geno = do.call(cbind,datalist)
-pop_wgeno = do.call(cbind,weightlist)
-write.csv(pop_geno, file="Population Genotypes G0")				#	//OPTIONAL 
+}
 
-prs <- colSums(pop_wgeno)
-quantile(prs,c(0.5))
-rm(pop_wgeno)
-rm(weightlist)
-pop_geno <- rbind(pop_geno,prs)
-Sub <- subset(pop_geno, , pop_geno[1001,] >= quantile(prs,c(0.2)))			#//SELECTION STEP
-Sub <- Sub[-1001,]
+# dataframe with simulated genotypes for num_individuals and SNPs provided in input data
+sim_geno_df <- data.frame(genolist)
+colnames(sim_geno_df) <- c(paste0("ID", c(1:num_individuals)))
+sim_geno_df$SNP <- mydata$SNP
 
-new_freq <- rowSums(Sub)/(2*80000)
-hist(prs, xlim=c(-3,5))
-t1 <- (1-new_freq)^2
-t2 <- 1-new_freq^2
-prob_dist <- exp(prs)/135.9
-plot(prs, prob_dist, xlim=c(-3,5), ylim=c(0,1))
+write.table(sim_geno_df, "Simulatedgenotypes.txt", row.names = F, col.names = T, quote = F, sep = "\t")
 
-sort_pd <- sort(prob_dist)
-rr <- runif(100000,0,1)
-case <- ifelse(rr<sort_pd,1,0)
-sum(case[1:100000])
-dlist = list()
-for(i in 1:50) {
-  sumcase <- sum(case[(2000*(i-1)+1):(2000*(i-1)+2000)])
-  dlist[[i]] <- sumcase }
-prevalences = do.call(cbind,dlist)/20
-percentile <- seq(1,50,by=1)
-plot(percentile,prevalences, ylim=c(0,30))
+#############################################################################################
 
-Exponential model
-prob_dist <- exp(prs*0.02)*0.000000004
-prob_dist <- exp(prs*0.03)*0.0000000000002			steeper
-prob_dist <- 0.1 + ifelse(prs<950, 0, exp(prs*0.03)*0.0000000000004)		higher prevalence
+# Check simulated genotypes
+# Check if allele frequency of simulated genotypes match with 
+# allele frequency provided in input real data
 
-Importing the Frequencies and Weights
+sim_geno_df$AF <- (rowSums(sim_geno_df))/(2*num_individuals)
+sim_geno_df$SNP <- rownames(sim_geno_df)
 
-mydata = read.csv("CADtest.csv")
-t1 <- (1-mydata$Freq)^2
-t2 <- 1-mydata$Freq^2
-set.seed(7251963)
+match_sim_actual <- merge(mydata, sim_geno_df, by = "SNP")
 
-genolist = list()
-weightlist = list()
-for(i in 1:100000) {
-  n1000 <- runif(1000,0,1)
-  geno <- ifelse(n1000<t1, 0, ifelse(n1000<t2,1,2))
-  wgeno <- ifelse(n1000<t1, 0, ifelse(n1000<t2,mydata$LogOdds,2*mydata$LogOdds))
-  genolist[[i]] <- geno
-  weightlist[[i]] <- wgeno }
-pop_geno = do.call(cbind,genolist)
-pop_wgeno = do.call(cbind,weightlist)
-write.csv(pop_geno, file="Population Genotypes G0")
+# Compare allele freqeuncies of simulated and real input data
+plot(match_sim_actual$Freq, match_sim_actual$AF, xlab = "Allele freq simulated data", ylab = "Allele freq real data")
+cor.test(match_sim_actual$Freq, match_sim_actual$AF)
 
-prs <- colSums(pop_wgeno)
-quantile(prs,c(0.5))
-pop_geno <- rbind(pop_geno,prs)
-Sub <- subset(pop_geno, , pop_geno[1001,] <= quantile(prs,c(0.8)))
-Sub <- Sub[-1001,]
-
-new_freq <- rowSums(Sub)/(2*50000)
-hist(prs, xlim=c(-3,5))
-t1 <- (1-new_freq)^2
-t2 <- 1-new_freq^2
-prob_dist <- exp(prs)/135.9
-plot(prs,prob_dist, xlim=c(-3,5), ylim=c(0,1))
-
-sort_pd <- sort(prob_dist)
-rr <- runif(100000,0,1)
-case <- ifelse(rr<sort_pd,1,0)
-sum(case[1:100000])
-dlist = list()
-for(i in 1:50) {
-  sumcase <- sum(case[(2000*(i-1)+1):(2000*(i-1)+2000)])
-  dlist[[i]] <- sumcase }
-prevalences = do.call(cbind,dlist)/20
-percentile <- seq(1,50,by=1)
-plot(percentile,prevalences, ylim=c(0,30))
-
+#############################################################################################
